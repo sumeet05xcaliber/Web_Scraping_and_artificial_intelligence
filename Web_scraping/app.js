e7402a6cecb84490eab83e837c07a5f0240e7de7
sumeet05xcaliber <ssumeet419@gmail.com>
2023-10-21 19:08:25 +0530

    scraping workin fine

--- Web_scraping/app.js
+++ Web_scraping/app.js
@@ -1,22 +1,27 @@
 import express from 'express'
 import cheerio from 'cheerio'
+import cors from 'cors'
+import axios from 'axios'
 const port=5000
 const app=express();
+app.use(cors())
+app.use(express.json())
 app.post('/scrape', async (req, res) => {
-    const { url } = req.body;
-  
-    try {
-      const response = await axios.get(url);
-      const $ = cheerio.load(response.data);
-      const scrapedText = $('body').text();
-  
-      // Send scraped text to the Flask server for processing
-      // (implement this in the next steps)
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Scraping failed' });
-    }
-  });
+  const { url } = req.body;
+
+  try {
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+    const title = $('title').text(); // Extract the title from the <title> tag
+    console.log(title); // Log the title to the console
+
+    // If you want to return the title as a response
+    res.status(200).json( title );
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Scraping failed' });
+  }
+});
 app.use(express.json())
 app.listen(port,()=>{
     console.log(`Node Js server is running on port ${port}`);
--- frontend/myapp/src/App.js
+++ frontend/myapp/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [url, setUrl] = useState('');
+  const [result, setResult] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Send the URL to the Node.js server for scraping
+      const response = await axios.post('http://localhost:5000/scrape', { url });
+
+      // Send the scraped text to the Flask server for processing
+      // const processingResponse = await axios.post('http://flask-server-url:5000/process-text', {
+      //   text: response.data.scrapedText,
+      // });
+
+      setResult(response.data);
+    } catch (error) {
+      console.error(error);
+      setResult('An error occurred');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Web Scraping and Text Processing</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter a URL"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <button type="submit">Scrape and Process</button>
+      </form>
+      {result && (
+        <div>
+          <h2>Result</h2>
+          <pre>{JSON.stringify(result, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
 }
